68db3e31da1f374990a13b14e8647968df7a32b4
TheoOliveira <akai.hen@gmail.com>
2020-06-20 00:53:34 -0300

    likes desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
- const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,25 +11,70 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
-  
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  const {title, url, techs} = request.body;
-  const repository = {id: uuid(), title, url, techs, likes: 0};
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+  return response.status(200).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "repositóŕio não encontrado!" });
+  }
+  const repository = { id, title, url, techs, likes };
+
+  repositories[repoIndex] = repository;
+  console.log(repository);
+  return response.status(200).json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "repositóŕio não encontrado!" });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(200).json({mensagem: 'repositório deletado!'});
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id } = request.params;
+  
+  const repoIndex = repositories.findIndex(
+    (repository) => {repository.id === id
+    
+  }
+  );
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "repositóŕio não encontrado!" });
+  }
+  console.log(repositories[repoIndex]);
+
+  repositories[repoIndex].likes++;  
+  
+
+  return response.status(200).json(repositories[repoIndex]);
 });
+
 
 module.exports = app;
